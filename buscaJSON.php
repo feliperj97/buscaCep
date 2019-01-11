88fb27f289535ef9e94ae73feb6b00d74fe4d292
Felipe Lamim <fclamim@gmail.com>
2019-01-11 13:14:24 -0200

    retornando em XML e JSON

--- buscaJSON.php
+++ buscaJSON.php
@@ -1,7 +1,8 @@
 <?php
 
    // $cep = preg_replace("/[^0-9]/", "", $cep);
-   $cep = "21020330";
+   
+   $cep = '21020330';
     $url = "https://viacep.com.br/ws/$cep/json";
     
     $client = curl_init($url);
@@ -11,6 +12,8 @@
     $response = curl_exec($client);
 
     $result = json_decode($response);
-    echo $result->logradouro;
-    
+  
+   echo $result->logradouro;
+   
+  
 ?>
